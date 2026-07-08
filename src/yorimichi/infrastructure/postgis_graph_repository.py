ca32151a0ca0de16f8ec4819bb974fecc1c3db83
victoriaghoos <ghoosvictoria@gmail.com>
2026-07-08 12:58:56 +0900

    Implement find_shortest_route and find_scenic_route methods in PostGISGraphRepository

--- src/yorimichi/infrastructure/postgis_graph_repository.py
+++ src/yorimichi/infrastructure/postgis_graph_repository.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 from yorimichi.domain.entities import Node, Route
 from yorimichi.domain.repositories import IGraphRepository
+from yorimichi.infrastructure.osmnx_routing_adapter import make_edge_weight_fn, make_heuristic_fn
 from yorimichi.infrastructure.postgis_models import NodeModel, EdgeModel
 
 
@@ -65,7 +66,19 @@ class PostGISGraphRepository(IGraphRepository):
             session.close()
 
     def find_shortest_route(self, graph, orig: Node, dest: Node) -> Route:
-        raise NotImplementedError("find_shortest_route() not yet implemented: coming in the next step")
+        orig_id, dest_id = orig.id, dest.id
+        path = nx.shortest_path(graph, orig_id, dest_id, weight="length")
+        length = nx.shortest_path_length(graph, orig_id, dest_id, weight="length")
+        return Route(node_ids=tuple(str(n) for n in path), length=length)
+
 
     def find_scenic_route(self, graph, orig: Node, dest: Node, scenic_provider) -> Route:
-        raise NotImplementedError("find_scenic_route() not yet implemented: coming in the next step")
+        orig_id, dest_id = orig.id, dest.id
+        weight_fn = make_edge_weight_fn(graph, scenic_provider)
+        heuristic_fn = make_heuristic_fn(graph)
+        path = nx.astar_path(graph, orig_id, dest_id, heuristic=heuristic_fn, weight=weight_fn)
+        length = sum(
+            graph.edges[path[i], path[i + 1], 0].get("length", 0)
+            for i in range(len(path) - 1)
+        )
+        return Route(node_ids=tuple(str(n) for n in path), length=length)
